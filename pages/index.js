d8f6ece0d90bda1609189565f6ea0ccff65d1575
luc-seidl <115326189+luc-seidl@users.noreply.github.com>
2025-10-14 12:43:07 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+function Home(){
+    // Título da Página
+    return <h1>Olá! bom dia.</h1>
+}
+
+export default Home;
+
+// nvm alias default
+// nvmrc requisitos
+// npm install next.js@13.1.6
+// npm install react@18.2.0
+// npm install react-dom@18.2.0
+
+
+
+// git status pra checar o status dos arquivos (aperta Q pra sair do (end))
+// git add <file> pra adicionar o arquivo pro stage
+// git commit pra gerar o commit e preparar pra subir
+    // -m "<mensagem>"
+    // --amend "emenda" o ultimo commit
+// git branch pra checar a branch
+// git push pra passar ao repositório de origem
+// git pull pra puxar arquivos do git
+
+// git log pra checar os logs de cada commit (add --oneline pra ser mais simples)
