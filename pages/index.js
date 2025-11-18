3747b624168532b0cbdaaad65051ec262094ec81
luc-seidl <115326189+luc-seidl@users.noreply.github.com>
2025-11-18 18:06:53 -0300

    Update Clarity

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function Home() {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "u87jj2");
+            })(window, document, "clarity", "script", "u87jj1jqy2");
           `,
         }}
       />
